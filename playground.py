26df1b8fdc6a280d8ba3ac2ebd746747b26dc714
Mitchell <mlbsmith15@hotmail.com>
2020-04-07 22:52:43 -0400

    add main app file, update layout

--- playground.py
+++ playground.py
@@ -1,375 +1,89 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import date, timedelta
 import requests
-from operator import add
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 from scipy.optimize import curve_fit
 from scipy.special import expit
 
-sns.set(palette='pastel')
+class Playground:
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
-app.config.suppress_callback_exceptions = True
+    def __init__(self):
+        return
 
-# set the app.layout
-app.layout = html.Div([
-    dcc.Tabs(id="tabs", value='ontario', children=[
-        dcc.Tab(label='Alberta', value='35'),
-        dcc.Tab(label='British Columbia', value='36'),
-        dcc.Tab(label='Manitoba', value='38'),
-        dcc.Tab(label='New Brunswick', value='39'),
-        dcc.Tab(label='Newfoundland & Labrador', value='40'),
-        dcc.Tab(label='Nova Scotia', value='41'),
-        dcc.Tab(label='Ontario', value='42'),
-        dcc.Tab(label='Prince Edward Island', value='43'),
-        dcc.Tab(label='Quebec', value='44'),
-        dcc.Tab(label='Saskatchewan', value='45'),
-        dcc.Tab(label='Canada', value='all'),
-    ]),
-    html.Div(id='tabs-content')
-])
+    def projected_value(self,daily_total_infections, day):
+        three_day_rate_of_growth = (daily_total_infections[day - 1] / daily_total_infections[day - 2] +
+                                    daily_total_infections[day - 2] / daily_total_infections[day - 3] +
+                                    daily_total_infections[day - 3] / daily_total_infections[day - 4]) / 3
+        projected_daily_total_infections = three_day_rate_of_growth * daily_total_infections[day - 1]
+        return projected_daily_total_infections, three_day_rate_of_growth
 
 
-def projected_value(daily_total_infections, day):
-    three_day_rate_of_growth = (daily_total_infections[day - 1] / daily_total_infections[day - 2] +
-                                daily_total_infections[day - 2] / daily_total_infections[day - 3] +
-                                daily_total_infections[day - 3] / daily_total_infections[day - 4]) / 3
-    projected_daily_total_infections = three_day_rate_of_growth * daily_total_infections[day - 1]
-    return projected_daily_total_infections, three_day_rate_of_growth
+    def sigmoid_projection(self,daily_total_infections):
+        def f_sigmoid(x, a, b, c):
+            # a = sigmoid midpoint
+            # b = curve steepness (logistic growth)
+            # c = max value
+            return c * expit(b * (x - a))
+
+        ydata = daily_total_infections
+        xdata = np.arange(0, len(ydata))
+
+        popt, pcov = curve_fit(f_sigmoid, xdata, ydata, [np.median(xdata)+20, 1, max(ydata)], method='lm')
+
+        one_stdev = np.sqrt(np.diag(pcov))
+        a_ymin = popt[0] + 3 * one_stdev[0]
+        b_ymin = popt[1] - 3 * one_stdev[1]
+        c_ymin = popt[2] - 3 * one_stdev[2]
+        a_ymax = popt[0] - 3 * one_stdev[0]
+        b_ymax = popt[1] + 3 * one_stdev[1]
+        c_ymax = popt[2] + 3 * one_stdev[2]
+
+        x_pred = np.arange(0, len(ydata) + 20)
+        y_pred = f_sigmoid(x_pred, popt[0], popt[1], popt[2])
+        y_pred_min = f_sigmoid(x_pred, a_ymin, b_ymin, c_ymin)
+        y_pred_max = f_sigmoid(x_pred, a_ymax, b_ymax, c_ymax)
+
+        sdate = date(2020, 1, 22)  # start date
+        day_list = []
+
+        for i in range(len(y_pred)):
+            day_list.append(sdate + timedelta(days=i))
+
+        return day_list, y_pred, y_pred_min, y_pred_max
 
 
-def sigmoid_projection(daily_total_infections):
-    def f_sigmoid(x, a, b, c):
-        # a = sigmoid midpoint
-        # b = curve steepness (logistic growth)
-        # c = max value
-        return c * expit(b * (x - a))
+    def plot_provincial_data(self,province_id):
+        url = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations/' + str(province_id)
 
-    ydata = daily_total_infections
-    xdata = np.arange(0, len(ydata))
+        # Connect to the URL
+        response = requests.get(url)
 
-    popt, pcov = curve_fit(f_sigmoid, xdata, ydata, [np.median(xdata)+20, 1, max(ydata)], method='lm')
+        json = response.json()
 
-    one_stdev = np.sqrt(np.diag(pcov))
-    a_ymin = popt[0] + 3 * one_stdev[0]
-    b_ymin = popt[1] - 3 * one_stdev[1]
-    c_ymin = popt[2] - 3 * one_stdev[2]
-    a_ymax = popt[0] - 3 * one_stdev[0]
-    b_ymax = popt[1] + 3 * one_stdev[1]
-    c_ymax = popt[2] + 3 * one_stdev[2]
+        timeline_array = json['location']['timelines']['confirmed']['timeline']
+        name = json['location']['province']
 
-    x_pred = np.arange(0, len(ydata) + 20)
-    y_pred = f_sigmoid(x_pred, popt[0], popt[1], popt[2])
-    y_pred_min = f_sigmoid(x_pred, a_ymin, b_ymin, c_ymin)
-    y_pred_max = f_sigmoid(x_pred, a_ymax, b_ymax, c_ymax)
+        base_daily_infections = []
+        prev_day = 0
 
-    sdate = date(2020, 1, 22)  # start date
-    day_list = []
+        for key in sorted(timeline_array.keys()):
+            base_daily_infections.append(timeline_array[key] - prev_day)
+            prev_day = timeline_array[key]
 
-    for i in range(len(y_pred)):
-        day_list.append(sdate + timedelta(days=i))
+        daily_infections = np.array(base_daily_infections)
 
-    return day_list, y_pred, y_pred_min, y_pred_max
+        sdate = date(2020, 1, 22)  # start date
+        edate = date.today() + timedelta(days=5)  # end date
 
+        delta = edate - sdate  # as timedelta
 
-def plot_provincial_data(province_id):
-    url = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations/' + str(province_id)
+        day_list = []
+        for i in range(delta.days + 1):
+            day_list.append(sdate + timedelta(days=i))
 
-    # Connect to the URL
-    response = requests.get(url)
+        daily_total_infections = []
+        for i in range(0, len(daily_infections)):
+            daily_total_infections.append(np.sum(daily_infections[0:i + 1]))
 
-    json = response.json()
+        day_list, y_pred, y_pred_min, y_pred_max = self.sigmoid_projection(daily_total_infections)
 
-    timeline_array = json['location']['timelines']['confirmed']['timeline']
-    name = json['location']['province']
-
-    base_daily_infections = []
-    prev_day = 0
-
-    for key in sorted(timeline_array.keys()):
-        base_daily_infections.append(timeline_array[key] - prev_day)
-        prev_day = timeline_array[key]
-
-    daily_infections = np.array(base_daily_infections)
-
-    sdate = date(2020, 1, 22)  # start date
-    edate = date.today() + timedelta(days=5)  # end date
-
-    delta = edate - sdate  # as timedelta
-
-    day_list = []
-    for i in range(delta.days + 1):
-        day_list.append(sdate + timedelta(days=i))
-
-    daily_total_infections = []
-    for i in range(0, len(daily_infections)):
-        daily_total_infections.append(np.sum(daily_infections[0:i + 1]))
-
-    day_list, y_pred, y_pred_min, y_pred_max = sigmoid_projection(daily_total_infections)
-
-    return daily_total_infections, day_list, y_pred, y_pred_min, y_pred_max
-
-
-# Start script
-print("Attempting to get data from coronavirus tracker api")
-
-sdate = date(2020, 1, 22)  # start date
-edate = date.today() + timedelta(days=5)  # end date
-delta = edate - sdate  # as timedelta
-
-day_list = []
-for i in range(delta.days + 1):
-    day_list.append(sdate + timedelta(days=i))
-
-all_province_case_data = {}
-all_province_projection_data = {}
-all_province_projection_data_min = {}
-all_province_projection_data_max = {}
-
-cumulative_total, day_list_pred, cumulative_projected_total, \
-cumulative_projected_total_min, \
-cumulative_projected_total_max = plot_provincial_data(35)
-
-all_province_case_data['35'] = cumulative_total
-all_province_projection_data['35'] = cumulative_projected_total
-all_province_projection_data_min['35'] = cumulative_projected_total_min
-all_province_projection_data_max['35'] = cumulative_projected_total_max
-
-# Set the URL of the REST api we are getting data from (i.e. location 42 is for the province of Ontario)
-for province_id in range(36, 46):
-    if (province_id == 37):
-        continue
-    all_province_case_data[str(province_id)], day_list_pred, all_province_projection_data[str(province_id)], \
-    all_province_projection_data_min[str(province_id)], \
-    all_province_projection_data_max[str(province_id)] = plot_provincial_data(province_id)
-    cumulative_total = list(map(add, cumulative_total, all_province_case_data[str(province_id)]))
-    cumulative_projected_total = list(
-        map(add, cumulative_projected_total, all_province_projection_data[str(province_id)]))
-    cumulative_projected_total_min = list(
-        map(add, cumulative_projected_total_min, all_province_projection_data_min[str(province_id)]))
-    cumulative_projected_total_max = list(
-        map(add, cumulative_projected_total_max, all_province_projection_data_max[str(province_id)]))
-
-
-def figure_creator(id):
-    return {
-        'data': [
-            dict(
-                y=all_province_projection_data[id],
-                x=day_list_pred,
-                mode='lines+markers',
-                opacity=0.4,
-                marker={
-                    'size': 8,
-                    'line': {'width': 0.5, 'color': 'gray'},
-                    'color': 'gray'
-                },
-                name='Projected Cases,'
-            ),
-            dict(
-                y=all_province_projection_data_max[id],
-                x=day_list_pred,
-                mode='none',
-                opacity=0.4,
-                marker={
-                    'size': 8,
-                    'line': {'width': 0.5, 'color': 'black'},
-                    'color': 'black'
-                },
-                showlegend=False,
-                name='Projected Error',
-                fill='tonexty',
-                fillcolor='rgba(0, 0, 0, 0)'
-            ),
-            dict(
-                y=all_province_projection_data_min[id],
-                x=day_list_pred,
-                mode='none',
-                opacity=0.4,
-                marker={
-                    'size': 8,
-                    'line': {'width': 0.5, 'color': 'black'},
-                    'color': 'black'
-                },
-                name='Projected Error',
-                fill='tonexty',
-                fillcolor='rgba(0, 0, 0, 0.1)'
-            ),
-            dict(
-                y=all_province_case_data[id],
-                x=day_list[0:-5],
-                mode='lines+markers',
-                opacity=1,
-                marker={
-                    'size': 8,
-                    'line': {'width': 0.5, 'color': 'red'},
-                    'color': 'red'
-                },
-                name='Confirmed Cases'
-            )
-        ],
-        'layout': dict(
-            xaxis={'type': 'date', 'title': 'Day'},
-            yaxis={'title': 'DAVID-19 Cases'},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
-        )
-    }
-
-
-# callback to control content
-@app.callback(Output('tabs-content', 'children'),
-              [Input('tabs', 'value')])
-def render_content(tab):
-    if tab == 'all':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure={
-                    'data': [
-                        dict(
-                            y=cumulative_projected_total,
-                            x=day_list_pred,
-                            mode='lines+markers',
-                            opacity=0.4,
-                            marker={
-                                'size': 8,
-                                'line': {'width': 0.5, 'color': 'gray'},
-                                'color': 'gray'
-                            },
-                            name='Projected Cases'
-                        ),
-                        dict(
-                            y=cumulative_projected_total_max,
-                            x=day_list_pred,
-                            mode='none',
-                            opacity=0.4,
-                            marker={
-                                'size': 8,
-                                'line': {'width': 0.5, 'color': 'black'},
-                                'color': 'black'
-                            },
-                            showlegend=False,
-                            name='Projected Error',
-                            fill='tonexty',
-                            fillcolor='rgba(0, 0, 0, 0)'
-                        ),
-                        dict(
-                            y=cumulative_projected_total_min,
-                            x=day_list_pred,
-                            mode='none',
-                            opacity=0.4,
-                            marker={
-                                'size': 8,
-                                'line': {'width': 0.5, 'color': 'black'},
-                                'color': 'black'
-                            },
-                            name='Projected Error',
-                            fill='tonexty',
-                            fillcolor='rgba(0, 0, 0, 0.1)'
-                        ),
-                        dict(
-                            y=cumulative_total,
-                            x=day_list[0:-5],
-                            mode='lines+markers',
-                            opacity=1,
-                            marker={
-                                'size': 8,
-                                'line': {'width': 0.5, 'color': 'red'},
-                                'color': 'red'
-                            },
-                            name='Days v Confirmed Cases'
-
-                        )
-                    ],
-                    'layout': dict(
-                        xaxis={'type': 'date', 'title': 'Day'},
-                        yaxis={'title': 'Confirmed Cases'},
-                        margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                        legend={'x': 0, 'y': 1},
-                        hovermode='closest'
-                    )
-                }
-            )
-        ])
-    elif tab == '35':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('35')
-            )
-        ])
-    elif tab == '36':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('36')
-            )
-        ])
-    elif tab == '38':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('38')
-            )
-        ])
-    elif tab == '39':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('39')
-            )
-        ])
-    elif tab == '40':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('40')
-            )
-        ])
-    elif tab == '41':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('41')
-            )
-        ])
-    elif tab == '42':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('42')
-            )
-        ])
-    elif tab == '43':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('43')
-            )
-        ])
-    elif tab == '44':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('44')
-            )
-        ])
-    elif tab == '45':
-        return html.Div([
-            dcc.Graph(
-                id='covid',
-                figure=figure_creator('45')
-            )
-        ])
-
-
-app.run_server()
+        return daily_total_infections, day_list, y_pred, y_pred_min, y_pred_max
